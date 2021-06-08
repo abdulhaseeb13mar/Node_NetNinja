d53d0e04c3645750fd522fa72a06cbfee3deea4b
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-06-08 11:44:48 +0500

    video 7 done

--- app.js
+++ app.js
@@ -2,20 +2,39 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Hello World",
+      snippet:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      title: "Hello World",
+      snippet:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      title: "Hello World",
+      snippet:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blog/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.render("404", { title: "404" });
 });
